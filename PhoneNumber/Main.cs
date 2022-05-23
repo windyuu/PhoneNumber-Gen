2ca512336d1575df8beca3f5547157efc8b0de2b
windyuu <78353602+windyuu@users.noreply.github.com>
2022-05-23 15:45:53 +0900

    Update Main.cs

--- PhoneNumber/Main.cs
+++ PhoneNumber/Main.cs
@@ -1,4 +1,4 @@
-﻿using static System.Console;
+using static System.Console;
 using System;
 
 namespace PhoneNumber
@@ -10,29 +10,28 @@ namespace PhoneNumber
 
 	public class Program
 	{
-		public void Method()
-        {
-            while (true)
+	    public void Method()
+	    {
+            	while (true)
             {
-				if (ReadLine() != ".")
-					Main(); break;
+		    if (ReadLine() != ".")
+			Main(); break;
             }
         }
 		
-		public static void Main()
+	public static void Main()
         {
-			string Number = "080";
-
-			Random random = new Random(); // 000.000.000.00
-
-			for (int phone = 0; phone < 8; phone++)
-            {
-				Number += random.Next(1, 9);
-            }
-			WriteLine(Number); WriteLine("続行するにはキーを押してください...");
-
-			Program program = new Program();
-			program.Method();
+		string Number = "080";
+		
+		Random random = new Random(); // 000.000.000.00
+		
+		for (int phone = 0; phone < 8; phone++)
+		{
+		    Number += random.Next(1, 9);
 		}
+		WriteLine(Number); WriteLine("続行するにはキーを押してください...");
+		Program program = new Program();
+		program.Method();
 	}
+    }
 }
